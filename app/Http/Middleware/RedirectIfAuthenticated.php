73d0787659e149c4de0112ce991ba6ba65313ffa
Vincentius Yudha R <92367937+118140170Vincentius@users.noreply.github.com>
2024-08-27 11:51:19 +0700

    fixed issue

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,12 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                 if(Auth::user()->hasRole('admin')){
+                    return redirect(RouteServiceProvider::HOME);
+                 }
+                 if(Auth::user()->hasRole('employee')){
+                    return redirect(RouteServiceProvider::EMPLOYE);
+                 }
             }
         }
 
